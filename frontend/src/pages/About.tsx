5e88bf989ff3f1734c8f52eaff245658d4ab63e0
Udasri Hasindu <144355134+u-sri2002@users.noreply.github.com>
2024-10-20 16:54:24 +0000

    add routes and components shadcn and ui

---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,12 @@
+
+const another = () =>{
+
+    return <p> Hello</p>
+}
+
+const About = () => {
+
+    return <h1>this is About</h1>
+}
+
+export {About as default, another};
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+
+
+function NotFound() {
+
+    return <h1>this page is not found</h1>
+}
+
+export default NotFound;
